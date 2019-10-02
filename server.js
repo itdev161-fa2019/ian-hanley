cf8e63a044270d03bd6f1720d1f2b8815bd682a6
Ian Hanley <razedonrock@gmail.com>
2019-10-02 18:51:03 -0500

    add

--- server.js
+++ server.js
@@ -7,8 +7,28 @@ const app = express();
 //connect database
 connectDatabase();
 
-//api endpoints
-app.get('/', (req, res) => res.send('http get request to root api endpoint'));
+//config middleware
+app.use(express.json({ extended: false}));
 
-//connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+//api endpoints
+/**
+ * @route GET/
+ * @desc test endpoint
+ */
+
+ app.get('/', (req, res) => 
+ res.send('http get request sent to root api endpoint')
+ );
+
+ /**
+  * @route POST api/users
+  * @desc register user
+  */
+
+  app.post('/api/users', (req, res) => {
+      console.log(req.body);
+      res.send(req.body);
+  });
+
+  //connection listener
+  app.listen(3000, () => console.log('Express server running on port 3000'));
