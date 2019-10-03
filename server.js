233e988187aeddd3fe982670b50825b782a646da
Ian Hanley <razedonrock@gmail.com>
2019-10-02 20:23:01 -0500

    stuff

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 //initiate express application
 const app = express();
@@ -25,9 +26,19 @@ app.use(express.json({ extended: false}));
   * @desc register user
   */
 
-  app.post('/api/users', (req, res) => {
-      console.log(req.body);
-      res.send(req.body);
+  app.post('/api/users', [
+      check('name', 'Please enter your name').not().isEmpty(),
+      check('email', 'Please enter a valid email').isEmail(),
+      check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6}) 
+      ,
+  ], (req, res) => {
+     const errors = validationResult(req);
+     if (!errors.isEmpty()) {
+         return res.status(422).json({ errors: errors.array()});
+     }
+     else {
+         return res.send(req.body);
+     }
   });
 
   //connection listener
