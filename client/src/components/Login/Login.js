98b497c12261b564e9909e3ae1804289525d46f6
Ian Hanley <razedonrock@gmail.com>
2019-11-13 14:06:00 -0600

    add

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
+const Login = ({ authenticateUser }) => {
+  let history = useHistory();
+  const [userData, setUserData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [errorData, setErrorData] = useState({ errors: null });
+  const { email, password } = userData;
+  const { errors } = errorData;
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setUserData({
+      ...userData, [name]: value
+    })
+  }
 }
 
 export default Login
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 const Register = () => {
   const [userData, setUserData] = useState({
@@ -10,6 +11,7 @@ const Register = () => {
   });
 
   const { name, email, password, passwordConfirm } = userData;
+  const { errors } = errorData; 
 
   const onChange = e => {
     const { name, value } = e.target;
